b99cbab2b8007b49a1976ae9865cae63b2cf28bd
shankar-010 <shankar.namaji3@gmail.com>
2025-07-07 23:10:47 +0530

    updated server

--- src/server.js
+++ src/server.js
@@ -228,7 +228,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: 'res-frontend-teal.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
